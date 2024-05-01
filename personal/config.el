35664eb5f5d9027158239434cd0e41228b568480
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-05-01 09:29:43 -0400

    update config

--- personal/config.el
+++ personal/config.el
@@ -2,10 +2,13 @@
 (direnv-mode)
 
 (setq cider-enrich-classpath t
-      cider-repl-pop-to-buffer-on-connect nil)
+      cider-repl-pop-to-buffer-on-connect nil
+      clojure-toplevel-inside-comment-form t)
 
 (set-frame-font "Berkeley Mono 12" nil t)
 
 (setq vc-follow-symlinks t)
+
+(setq company-idle-delay 0.2)
 
 (server-start)
